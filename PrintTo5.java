bf036822ae91080dce8ff8c357da0fc3ca45d893
Emma <emma.shemtov@runi.ac.il>
2025-11-03 16:20:41 +0200

    Ascend.java

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,12 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        System.out.println(i);
+        while (i<5){
+           i = i+1;
+           System.out.println(i);
+        }
+        System.out.println("Done");
         // Complete the program's code here:
     }
  }
